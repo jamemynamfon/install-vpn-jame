5e9802d5c3e48dd98fa326651db4aa6c0437421f
jamemynamfon <jamemynamfon@gmail.com>
2017-06-28 18:40:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade -y
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.0.list
 echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10
@@ -9,7 +9,7 @@ sudo iptables -A INPUT -p udp -m udp --sport 9700 --dport 1025:65355 -j ACCEPT
 sudo iptables -A INPUT -p tcp -m tcp --sport 9700 --dport 1025:65355 -j ACCEPT
 sudo apt-get --assume-yes install pritunl mongodb-org
 sudo service pritunl start
-sudo apt-get install squid3
+sudo apt-get install squid3 -y
 cd /etc/squid3
 wget https://pastebin.com/raw/a8vZRERB -O squid.conf
 sudo service squid3 restart
